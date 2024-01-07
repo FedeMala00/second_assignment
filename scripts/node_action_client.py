a0750428eb4855af1604e2839f7969926c2354b0
FedeMala00 <feffemala@gmail.com>
2024-01-07 12:26:37 +0100

    added comments

--- scripts/node_action_client.py
+++ scripts/node_action_client.py
@@ -28,14 +28,16 @@ def action_client():
     global x
     global y 
     # Creates the SimpleActionClient, passing the type of the action
-    # (FibonacciAction) to the constructor.
+    # (PlanningAction) to the constructor.
     client = actionlib.SimpleActionClient('/reaching_goal', second_assignment.msg.PlanningAction)
 
     # Waits until the action server has started up and started
     # listening for goals.
     client.wait_for_server()
+    # Creates a variable to check if the goal has been reached and another one to check if the goal has been cancelled
     var = True
     goal_reached = False
+    # Loop untill rospy is shutdown
     while not rospy.is_shutdown():
 
         # Creates a subscriber to the topic /odom positioned here because otherwise it won't be executed 
@@ -47,23 +49,25 @@ def action_client():
 
             x = float(input("Enter desired x coordinate: "))
             y = float(input("Enter desired y coordinate: "))
-            # Creates a goal to send to the action server.
-
+            
+            # Sets the parameters modifing the launch file
             rospy.set_param('/des_pos_x', x)
             rospy.set_param('/des_pos_y', y)
-            
+
+            # Creates a goal to send to the action server.
             goal = second_assignment.msg.PlanningGoal()
 
             goal.target_pose.pose.position.x = x
             goal.target_pose.pose.position.y = y
 
-            # Sends the goal to the action server
+            # Sends the goal to the action server and call the feedback_cb function
             client.send_goal(goal, feedback_cb=feedback_cb)
 
             # Used to print the change of status before the cancel_goal
             time.sleep(1) 
             cancel_goal = input("\nPress c if you want to cancel the goal and any other key to mantain it: ")
             if cancel_goal == 'c':
+                # Cancels the goal
                 client.cancel_goal()
                 rospy.loginfo("Goal cancelled")
                 time.sleep(0.1) 
@@ -73,7 +77,9 @@ def action_client():
         # Condition to check if the goal has been reached
         time.sleep(2)
         if goal_reached == False:
+            # Prints the latest feedback which is the actual position and orientation of the robot
             rospy.loginfo(latest_feedback)
+            # Condition to check if the robot is in the range of 0.5 from the goal to consider it reached
             if latest_feedback.actual_pose.position.x - 0.5 < x < latest_feedback.actual_pose.position.x + 0.5 and latest_feedback.actual_pose.position.y - 0.5 < y < latest_feedback.actual_pose.position.y + 0.5:
                 rospy.loginfo("Goal reached")
                 goal_reached = True
@@ -88,6 +94,7 @@ def action_client():
 
 def pub_pos_vel(message):
 
+    # Creates a publisher to the topic /pos_vel_topic which uses the custom message type Pos_vel
     pub = rospy.Publisher('pos_vel_topic', Pos_vel, queue_size=1)
 
     # Obtained by looking at the message type of the topic /odom (Odometry message) 
